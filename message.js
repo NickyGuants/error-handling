0235beaff79e27b67f3e123dcc96b7c56525cfa5
NickyGuants <nicholasguantai528@gmail.com>
2021-11-29 18:50:23 +0300

    first commit

---
+++ message.js
@@ -0,0 +1,10 @@
+const message = {
+    from: {
+        name: 'Guantai',
+        address: 'gnmutembei99@gmail.com'
+    },
+    to: 'nicholasguantai528@gmail.com',
+    subject: "ERROR NOTIFICATION",
+    text: "Hello, we are sending theis email to infor you that there is an error on your application.",
+}
+module.exports = message;
